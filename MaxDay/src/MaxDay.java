099cb338f0d068afc29785cb8053e82e0af47e13
bronzeq <36151742+bronzeq@users.noreply.github.com>
2020-01-22 17:57:54 +0900

    Add calender

--- MaxDay/src/MaxDay.java
+++ MaxDay/src/MaxDay.java
@@ -5,6 +5,17 @@ public class MaxDay {
 	private static  final int[] MAX_DAYS= {31,28,31,30,31,30,31,31,30,31,30,31};
 	public  int GetMonthOfMaxDays(int month){return MAX_DAYS[month-1];}
 	
+	public void samplecalendar()
+	{
+		System.out.println(
+				"일 월 화 수 목 금 토\r\n" + 
+				"--------------------\r\n" + 
+				" 1  2  3  4  5  6  7\r\n" + 
+				" 8  9 10 11 12 13 14\r\n" + 
+				"15 16 17 18 19 20 21\r\n" + 
+				"22 23 24 25 26 27 28");		
+	}
+ 
 
 	public static void main(String[] args) {
 		MaxDay day=new MaxDay();
@@ -13,8 +24,8 @@ public class MaxDay {
 		int month=scanner.nextInt();
 		
 		
-		System.out.printf("%d요일의 최대 날은 %d이다.",month,day.GetMonthOfMaxDays(month));
-		
+		System.out.printf("%d요일의 최대 날은 %d이다.\n",month,day.GetMonthOfMaxDays(month));
+		day.samplecalendar();
 		scanner.close();
 		
 	}
