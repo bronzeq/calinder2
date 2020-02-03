f6d4aafe61709837dccd07366b7632664198790a
bronzeq <36151742+bronzeq@users.noreply.github.com>
2020-02-03 12:32:22 +0900

    get of WeekDay

--- calinder/src/calinder/calinder.java
+++ calinder/src/calinder/calinder.java
@@ -12,24 +12,38 @@ public class calinder {
 		else return false;
 	}
 
-	public int  GetMaxDay(int year, int month) {
+	public int  GetMaxDay(int year, int month,int WeekDay) {
 		if(isLeepYear(year)){return LEEP_MAX_DAYS[month-1];}
 		else
 			return MAX_DAYS[month-1];
 		
 	}
 	
-	public void PrintCalinder(int year, int month)
+	public void PrintCalinder(int year, int month, int WeekDay)
 	{
 		 System.out.printf("      <<%4d년 %3d월>> \n",year, month);
-		System.out.println(	"SU  MO  TU  WE   TH  FR  SA"); 
-		System.out.println("----------------------------------" );
-		int Maxday= GetMaxDay(year,month);
-		for(int i=1;i<=Maxday;i++) {
-		System.out.printf("%2d  ",i);	
-		if (i%7==0) System.out.println();
+		System.out.println(	" SU   MO   TU   WE    TH   FR   SA"); 
+		System.out.println("------------------------------------" );
+		//print blink
+		for(int i=0;i<WeekDay;i++) {
+			System.out.print("     ");		
+		}
+		int Maxday= GetMaxDay(year,month,WeekDay);
+		int count =7-WeekDay;
+		int delim=(count<7)?count:0;
+		//F_LINE
+		for(int i=1;i<=count;i++) {
+			System.out.printf("%3d  ",i);	
+		}
+		System.out.println();
+		//print 2~L_LINE
+		for(int i=count+1;i<=Maxday;i++) {
+		System.out.printf("%3d  ",i);	
+		
+		if (i%7==delim) System.out.println();
 		
 		}System.out.println();
+		System.out.println();
 		
 	}
 		
--- calinder/src/calinder/prompt.java
+++ calinder/src/calinder/prompt.java
@@ -3,7 +3,27 @@ package calinder;
 import java.util.Scanner;
 
 public class prompt {
-	private final static String PROMPT="cal>";
+	
+	
+	/**
+	 * 
+	 * @param week=weekday
+	 * @return 0~6(0=SunDAy 6=SaturDAy)
+	 */
+	public int ParseDay (String week) {
+		if (week.equals( "su"))return 0;
+		else if(week.equals( "mo"))return 1;
+		else if(week.equals( "tu"))return 2;
+		else if(week.equals( "we"))return 3;
+		else if(week.equals( "th"))return 4;
+		else if(week.equals( "fr"))return 5;
+		else if(week.equals( "sa"))return 6;
+		else return 0;
+		
+		
+	}
+	
+	//private final static String PROMPT="cal>";
 	
 	public void runPROMPT() 
 	{
@@ -11,16 +31,25 @@ public class prompt {
 		calinder cal=new calinder();
 		int month=1;
 		int year=2000;
+		int WeekDay=0;
 		while(true) {
-			System.out.println("년 입력");
+			System.out.println("년 입력(exit==-1)");
 			System.out.print("year>");
 			year=scanner.nextInt();
+			if(year==-1)break;
+			
 			System.out.println("달 입력");
 			System.out.print("month>");
 			month=scanner.nextInt();
+			
+			System.out.println("첫째 날의 요일을 입력하시오(SU  MO  TU  WE   TH  FR  SA)");
+			System.out.print("WeekDay>");
+			String str_WeekDay=scanner.next();			
+			WeekDay= ParseDay(str_WeekDay);
+		
 			if(month==-1)  break;
 			if(month>12)continue;			
-			cal.PrintCalinder(year, month);
+			cal.PrintCalinder(year, month,WeekDay);
 			}
 		System.out.println("bye");
 		
